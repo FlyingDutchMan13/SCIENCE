50487585c29427e73046584fa8779b3ec52a63bb
MarkTempelman <43167879+MarkTempelman@users.noreply.github.com>
2020-10-20 17:40:32 +0200

    Added camera movement
    Added camera movement

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public PlayerController player;
+    public float cameraHeight;
+
+    void Update()
+    {
+        Vector3 position = player.transform.position;
+        position.y += cameraHeight;
+        transform.position = position;
+    }
+}
