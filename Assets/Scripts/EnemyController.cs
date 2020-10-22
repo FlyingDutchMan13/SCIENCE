2171496c76fb17aa9f33775084a0103caa634274
B1GgesTb0t <raffamuzakki06@gmail.com>
2020-10-22 16:46:39 +0700

    Added Enemy movement

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.UIElements;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public float moveSpeed;
+    private Rigidbody myRigidbody;
+    public PlayerController player;
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody>();
+        
+    }
+
+    
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        transform.LookAt(player.gameObject.transform.position);
+        myRigidbody.velocity = transform.forward * moveSpeed;
+
+    }
+}
