cfb9f8017ecd39b73e421e9241a9a6a15b78f06c
B1GgesTb0t <raffamuzakki06@gmail.com>
2020-10-22 18:01:56 +0700

    Enemy can attack player

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -8,22 +8,41 @@ public class EnemyController : MonoBehaviour
     public float moveSpeed;
     private Rigidbody myRigidbody;
     public PlayerController player;
+    public int damageToGive;
+    public float timeBetweenAttacks;
+    private float attackCounter;
+    private bool touchingPlayer;
     void Start()
     {
         myRigidbody = GetComponent<Rigidbody>();
-        
+        attackCounter = 0f;
     }
 
     
     void Update()
     {
-
+        attackCounter -= Time.deltaTime;
     }
 
     void FixedUpdate()
     {
         transform.LookAt(player.gameObject.transform.position);
         myRigidbody.velocity = transform.forward * moveSpeed;
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.GetComponent<PlayerController>() != null)
+        {
+            if (attackCounter <= 0f)
+            {
+                player.gameObject.GetComponent<HealthManager>().Damage(damageToGive);
+                attackCounter = timeBetweenAttacks;
+
+
+            }
+        }
 
     }
 }
---
+++ Assets/Scripts/HealthManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthManager : MonoBehaviour
+{
+    public int health;
+    private int currentHealth;
+    void Start()
+    {
+        currentHealth = health;
+
+    }
+
+    
+    void Update()
+    {
+        if (currentHealth <= 0)
+        {
+            KillPlayer();
+
+
+        }
+    }
+
+    public void Damage(int damage)
+    {
+        currentHealth -= damage;
+
+    }
+
+    private void KillPlayer()
+    {
+        Destroy(gameObject);
+
+    }
+
+}
